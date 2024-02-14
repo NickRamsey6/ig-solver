51d5ad093448cebf4b9f247155dddd582d920b14
Nick Ramsey <nramseysc@gmail.com>
2024-02-14 01:46:23 -0700

    sort both tables by total games played

--- scripts.py
+++ scripts.py
@@ -2,15 +2,50 @@ import pandas as pd
 
 
 def query_br(team1, team2):
+    teams_dict = {
+        'ANA':'Los Angeles Angels of Anaheim',
+        'ARI':'Arizona Diamondbacks',
+        'ATL':'Atlanta Braves',
+        'BAL':'Baltimore Orioles',
+        'BOS':'Boston Red Sox',
+        'CHC':'Chicago Cubs',
+        'CHW':'Chicago White Sox',
+        'CIN':'Cincinnati Reds',
+        'CLE':'Cleveland Guardians',
+        'COL':'Colorado Rockies',
+        'DET':'Detroit Tigers',
+        'FLA':'Florida Marlins',
+        'HOU':'Houston Astros',
+        'KCR':'Kansas City Royals',
+        'LAD':'Los Angeles Dodgers',
+        'TBD':'Tampa Bay Rays'
+    }
     url = 'https://www.baseball-reference.com/friv/players-who-played-for-multiple-teams-franchises.fcgi?level=franch&t1={}&t2={}&t3=--&t4=--&utm_campaign=2023_07_ig_possible_answers&utm_source=ig&utm_medium=sr_xsite'
     url = url.format(team1, team2)
     tables = pd.read_html(url)
     batting_table = tables[0]
     pitching_table = tables[1]
 
-    # Sort table by games played
-    print(batting_table.sort_values([('San Diego Padres', 'G')], ascending=True))
+    # Sort batting table by sum of games played for each team
+    batting_table['sum'] = batting_table[(teams_dict[team1], 'G')] + batting_table[(teams_dict[team2], 'G')]
+    sorted_batting = batting_table.sort_values([('sum', '')], ascending=True)
+    rarest_batter ={
+        'Name':sorted_batting[('Unnamed: 0_level_0', 'Name')].iloc[0],
+        'Games':sorted_batting[('sum', '')].iloc[0]
+    }
 
+    # Sort pitching table by sum of games played for each team
+    pitching_table['sum'] = pitching_table[(teams_dict[team1], 'G')] + pitching_table[(teams_dict[team2], 'G')]
+    sorted_pitching = pitching_table.sort_values([('sum', '')], ascending=True)
+    rarest_pitcher ={
+        'Name':sorted_pitching[('Unnamed: 0_level_0', 'Name')].iloc[0],
+        'Games':sorted_pitching[('sum', '')].iloc[0]
+    }
+
+    print(rarest_batter['Name'])
+    print(rarest_batter['Games'])
+    print(rarest_pitcher['Name'])
+    print(rarest_pitcher['Games'])
     return None
 
 print('Enter Team1 Abbr:')
