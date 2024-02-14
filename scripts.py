3e73eb5bacc388e0d6f27592ac9e6540f0348269
Nick Ramsey <nramseysc@gmail.com>
2024-02-13 23:41:54 -0700

    return player tables from queries

--- scripts.py
+++ scripts.py
@@ -1,14 +1,17 @@
 import numpy as np
 import pandas as pd
-from bs4 import BeautifulSoup
+import bs4
 import requests
 import sys
 
 def query_br(team1, team2):
     url = 'https://www.baseball-reference.com/friv/players-who-played-for-multiple-teams-franchises.fcgi?level=franch&t1={}&t2={}&t3=--&t4=--&utm_campaign=2023_07_ig_possible_answers&utm_source=ig&utm_medium=sr_xsite'
     url = url.format(team1, team2)
-    s = requests.get(url).content
-    print(s)
+    page = requests.get(url)
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
+    tables = [element.text for element in soup.find_all('tbody')]
+
+    print(tables)
 
 print('Enter Team1 Abbr:')
 team1 = input()
